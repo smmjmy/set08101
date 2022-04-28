c60cf5673f0cc08945556e26bfaf38466842962a
smmjmy <98585758+smmjmy@users.noreply.github.com>
2022-04-28 19:42:43 +0100

    Update name.js

--- Coursework/name.js
+++ Coursework/name.js
@@ -1,14 +1,14 @@
-
-
+/* saves the user's name in a cookie */
 function getInputValue(){
 	var name = document.getElementById("myInput").value;
 	document.cookie = name;
 }
 
-
+/* accesses the cookie and if it contains the name writes the message to the webpage */
 function output() {
 	let name = document.cookie;
 	if (name) {
 		document.getElementById("nameText").innerHTML = "Carved out with painstaking care it reads: " + name;
 	}
 }
+
